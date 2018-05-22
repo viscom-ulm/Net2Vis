d11557584ec03c9fce84274f1cf0d67ce16c16e5
Alex Workstation <alex.baeuerle@uni-ulm.de>
2018-05-22 13:44:54 +0200

    Comments for translate.py.

--- translate/translate.py
+++ translate/translate.py
@@ -1,6 +1,7 @@
 import argparse
 from keras import translate_keras
 
+# Define the Arguments needed.
 parser = argparse.ArgumentParser(
     description='Translate a network implementation into NetViz format.')
 parser.add_argument('file_name', metavar='file_name', type=str, 
@@ -8,10 +9,10 @@ parser.add_argument('file_name', metavar='file_name', type=str,
 parser.add_argument('model_type', metavar='model_type', type=str,
     help='Type of the model to be translated (currently supported: Keras)')
 
-args = parser.parse_args()
-file_input = open(args.file_name, 'r')
-content = file_input.readlines()
-content = [x.strip() for x in content] 
+args = parser.parse_args() # Parse the Arguments.
+file_input = open(args.file_name, 'r') # Get the input File.
+content = file_input.readlines() # Read the Input File
+content = [x.strip() for x in content] # Strip the input File Lines.
 
-if(args.model_type == 'Keras'):
+if(args.model_type == 'Keras'): # Keras-based Translation.
     translate_keras(content)
