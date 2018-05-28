e64b7ed2068738165ea6aa2741809ec845d0ba6c
Alex Workstation <alex.baeuerle@uni-ulm.de>
2018-05-28 15:47:15 +0200

    Remove D3.

--- netviz/app/js/netviz.js
+++ netviz/app/js/netviz.js
@@ -1,8 +1,5 @@
 import $ from 'jquery';
-const d3 = require('d3');
 
 export default function netviz() {
     var mainDiv = document.getElementById('mainContainer');
-    var svgNetViz = d3.select(mainDiv).append('svg');
-    console.log('test')
 }
