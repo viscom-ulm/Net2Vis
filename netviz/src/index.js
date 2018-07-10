565d2c8c5e0a778f59c7870608e4932973b674b6
Alex Workstation <alex.baeuerle@uni-ulm.de>
2018-07-10 08:23:06 +0200

    Remove Bootstrap stuff again.

--- netviz/src/index.js
+++ netviz/src/index.js
@@ -5,8 +5,6 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import './styles/index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 
 // Render the Application into the html 'root' element
 ReactDOM.render(<App />, document.getElementById('root'));
