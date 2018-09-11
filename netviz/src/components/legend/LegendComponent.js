ef8105cdffde45aeb9b3984366abcf4397b29299
Alex Workstation <alex.baeuerle@uni-ulm.de>
2018-09-11 07:33:51 +0200

    First Version of the Legend finished.

--- netviz/src/components/legend/LegendComponent.js
+++ netviz/src/components/legend/LegendComponent.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import LegendItem from './LegendItem';
 
 class Legend extends React.Component {
   render() {
     if(this.props.legend_toggle) {
+      const layer_types_settings = this.props.layer_types_settings;
+      var settings = [];
+      for (var key in layer_types_settings) {
+        settings.push({name: key, color: layer_types_settings[key].color});
+      }
       return(
-        <div id='Legend'/>
+        <div id='Legend'>
+          <div className='flexhorizontal flexlegend'>
+          {settings.map(setting => 
+            <LegendItem layer_name={setting.name} layer_color={setting.color} key={setting.name}/>
+          )}
+          </div>
+        </div>
       );
     } else {
       return null;
@@ -15,12 +27,14 @@ class Legend extends React.Component {
 }
 
 Legend.propTypes = {
-  legend_toggle: PropTypes.bool.isRequired
+  legend_toggle: PropTypes.bool.isRequired,
+  layer_types_settings: PropTypes.object.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
   return {
-    legend_toggle: state.display.legend_toggle
+    legend_toggle: state.display.legend_toggle,
+    layer_types_settings: state.layer_types_settings
   };
 }
 
---
+++ netviz/src/components/legend/LegendItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// ToggleButton Control Element appearance dependant on State of the Button. Action that is provided gets called on click.
+const LegendItem = ({layer_name, layer_color}) => {
+  const style = {
+    backgroundColor: layer_color
+  };  
+  return (
+    <div className='legendItem' style={style}>{layer_name}</div>
+  );
+};
+
+// Proptypes of ToggleBurttons
+LegendItem.propTypes = {
+  layer_name: PropTypes.string.isRequired,
+  layer_color: PropTypes.string.isRequired,
+};
+
+export default LegendItem;
