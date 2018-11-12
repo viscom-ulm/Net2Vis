ddff20d695d8d9494ac601734a46c9a061def461
Alex Workstation <alex.baeuerle@uni-ulm.de>
2018-11-12 14:54:04 +0100

    LayerItem changed to be its own component.
    The LayerItem is now a real Component as well, to support more functionality.

--- netviz/src/components/legend/LegendItem.js
+++ netviz/src/components/legend/LegendItem.js
@@ -1,23 +1,40 @@
 import React from 'react';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
+import {bindActionCreators} from 'redux';
+
+import * as actions from '../../actions';
 
 // <div className='legendItem' style={style} onClick={() => action(layer_name)}>{layer_name}</div>
 // ToggleButton Control Element appearance dependant on State of the Button. Action that is provided gets called on click.
-const LegendItem = ({layer_name, layer_color, action}) => {
-  const style = {
-    fill: layer_color,
-    stroke: 'black',
-    stokeLinejoin: 'round'
-  };  
-  return (
-    <rect x='10' y='10' width='20' height='80' style={style} onClick={() => action(layer_name)}/>
-  );
-};
+class LegendItem extends React.Component {
+  render() {
+    const style = {
+      fill: this.props.layer_color,
+      stroke: 'black',
+      stokeLinejoin: 'round'
+    };  
+    return (
+      <rect x='10' y='10' width='20' height='80' style={style} onClick={() => this.props.action(this.props.layer_name)}/>
+    ) 
+  }
+}
 
-// Proptypes of ToggleBurttons
+// PropTypes of this Class
 LegendItem.propTypes = {
-  layer_name: PropTypes.string.isRequired,
-  layer_color: PropTypes.string.isRequired,
-};
+  layer_types_settings: PropTypes.object.isRequired
+}
 
-export default LegendItem;
+// Map the State of the Application to the Props of this Class
+function mapStateToProps(state, ownProps) {
+  return {
+    layer_types_settings: state.layer_types_settings
+  };
+}
+
+// Map the Actions for the State to the Props of this Class
+function mapDispatchToProps(dispatch) {
+  return {actions: bindActionCreators(actions, dispatch)}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(LegendItem);
