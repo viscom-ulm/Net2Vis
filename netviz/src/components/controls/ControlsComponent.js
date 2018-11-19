ca123d1f5a81504b19be9657b6a74793a658ed34
Alex Workstation <alex.baeuerle@uni-ulm.de>
2018-11-19 16:39:58 +0100

    Legend Downloadable
    The Legend can now be downloaded as well. This includes correct size calculation and SVG composition.

--- netviz/src/components/controls/ControlsComponent.js
+++ netviz/src/components/controls/ControlsComponent.js
@@ -16,13 +16,22 @@ import groupLogo from '../../media/group_icon.png';
 class Controls extends React.Component {
   // Triggers download functionality of the network graph
   downloadSVG = () => {
-    var graph_element = document.getElementById('main_group');
-    var graph_width = graph_element.getBBox().width * this.props.group_transform.scale;
-    var graph_height = graph_element.getBBox().height * this.props.group_transform.scale;
-    var svg_text = graph_element.innerHTML; // Get the inner elements of the svg
-    const transform = `scale(${this.props.group_transform.scale}) translate(0, ${- (graph_element.getBBox().height/2.0)})`;
-    svg_text = "<svg version='1.1' baseProfile='full' xmlns='http://www.w3.org/2000/svg' width='" + (graph_width + 10) + "' height='" + (graph_height + 10) + "'><g transform='" + `translate(5,5)` + "'><g transform='" + transform + "'>" + svg_text + "</g></g></svg>"; // append svg tag
-    saveAs(new Blob([svg_text], {type: "text/svg;charset=utf-8"}), 'model.svg'); // save the svg on disk
+    var graph_element = document.getElementById('main_group'); // Get the main SVG Element
+    var graph_width = graph_element.getBBox().width * this.props.group_transform.scale; // Calculate the width of this Element
+    var graph_height = graph_element.getBBox().height * this.props.group_transform.scale; // Calculate the height of this Element
+    var graph_text = graph_element.innerHTML; // Get the inner elements of the svg
+    const graph_transform = `scale(${this.props.group_transform.scale}) translate(0, ${- (graph_element.getBBox().height/2.0)})`; // Set the transform for the graph to match the scaling and be centred
+    var graph_downloadable = "<svg version='1.1' baseProfile='full' xmlns='http://www.w3.org/2000/svg' width='" + (graph_width + 10) + "' height='" + (graph_height + 10) + "'><g transform='" + `translate(5,5)` + "'><g transform='" + graph_transform + "'>" + graph_text + "</g></g></svg>"; // Wrap together the svg code for the Graph
+
+    var legend_element = document.getElementById('legend_group'); // Get the legend SVG Element
+    var legend_width = legend_element.getBBox().width; // Calculate the width of this Element
+    var legend_height = legend_element.getBBox().height; // Calculate the height of this Element
+    var legend_text = legend_element.innerHTML; // Get the inner elements of the svg
+    const legend_transform = `scale(${this.props.group_transform.scale})`; // Set the transform for the legend to match the scaling and be centred
+    var legend_downloadable = "<svg version='1.1' baseProfile='full' xmlns='http://www.w3.org/2000/svg' width='" + (legend_width + 10) + "' height='" + (legend_height + 10) + "'><g transform='" + `translate(5,5)` + "'><g transform='" + legend_transform + "'>" + legend_text + "</g></g></svg>"; // Wrap together the svg code for the Legend
+
+    saveAs(new Blob([graph_downloadable], {type: "text/svg;charset=utf-8"}), 'model.svg'); // save the svg on disk
+    saveAs(new Blob([legend_downloadable], {type: "text/svg;charset=utf-8"}), 'legend.svg'); // save the svg on disk
   }
 
   // Group some Layers together
