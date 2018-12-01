907e2a455cfd647796879652a48c3034b272a300
Alex Bäuerle <alex.baeuerle@uni-ulm.de>
2018-12-01 12:21:14 +0100

    Remove adding logic from Group Reducer
    There was some logic in the group reducer that handled adding groups. This has been moved to prior to the reducer.

--- netviz/src/reducers/GroupsReducer.js
+++ netviz/src/reducers/GroupsReducer.js
@@ -1,14 +1,12 @@
 import initialState from './initialState';
 import * as types from '../actions/types';
-import * as addition from '../groups/Addition';
 
 export default function groupsReducer(state = initialState.groups, action) {
   switch (action.type) {
     case types.LOAD_GROUPS_SUCCESS:
       return action.groups;
     case types.ADD_GROUP:
-      state = addition.groupAdded(state, action.group);
-      return state.concat(action.group);
+      return action.groups;
     case types.UPDATE_GROUPS:
       return action.groups;
     default:
