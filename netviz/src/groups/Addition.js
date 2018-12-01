1b11e55771126aebc3f154a9de9e4204a74283a5
Alex Bäuerle <alex.baeuerle@uni-ulm.de>
2018-12-01 12:26:05 +0100

    Handles Logic of Adding a new Group
    When a new Group is added, this takes care of all that happens and updates the Groups.

--- netviz/src/groups/Addition.js
+++ netviz/src/groups/Addition.js
@@ -2,13 +2,12 @@ import * as occurences from './Occurences';
 import * as concatenation from './Concatenation';
 
 // A group was added to the Network, so the groups might need to also get grouped
-export function groupGroupLayers(groups, group) {
-  var newState = JSON.parse(JSON.stringify(groups)); // Copy the groups state
-  for (var i in newState) { // Check each Group
-    var groupOccur = occurences.findGroupOccurences(group, newState[i]); // If the new Group occurs in it
-    for (var j in occurences) { // For all Occurences
-      newState[i] = concatenation.concatenateLayers(occurences[j], newState[i], group); // Concatenate the Occurence
+export function addGroup(groups, group) {
+  for (var i in groups) { // Check each Group
+    var groupOccur = occurences.findGroupOccurences(group, groups[i]); // If the new Group occurs in it
+    for (var j in groupOccur) { // For all Occurences
+      groups[i] = concatenation.concatenateLayers(occurences[j], groups[i], group); // Concatenate the Occurence
     }
   }
-  return newState; // Return the new groups state
+  groups.push(group); // Add the new Group to the existing ones
 }
