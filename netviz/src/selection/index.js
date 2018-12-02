8d68fe815cfcee12859421818dc0c77a1ce0d6da
Alex <alex.baeuerle@uni-ulm.de>
2018-12-02 17:36:06 +0100

    Fixed an error in the Path calculation
    When a new way was found, it also contained the new Layer of the standard way. This has been fixed by adding Layers at the beginning of the Path calculation.

--- netviz/src/selection/index.js
+++ netviz/src/selection/index.js
@@ -1,12 +1,13 @@
 // Get all paths from a node s to node d in the network
 export function allPaths(s, d, network) {
   var paths = []; // Initially, no Paths found
-  traverseWay(s, d, network, [s], paths); // Traverse the way from s on
+  traverseWay(s, d, network, [], paths); // Traverse the way from s on
   return paths;
 }
 
 // Traverse the Network from node u on
 function traverseWay(u, d, network, path, paths) {
+  path.push(u);
   if (u.id === d.id) { // If u is the destination
     paths.push(path); // Add the path to the pathes
   } else if (u.properties.output.length === 0) { // If an output node has been reached
@@ -15,12 +16,10 @@ function traverseWay(u, d, network, path, paths) {
     for (var i = 0; i < u.properties.output.length; i++) { // For all Outputs of the current node
       if (i === 0) { // For the first of all outputs
         var nextLayer = network.layers[u.properties.output[i]]; // Get the next Layer
-        path.push(nextLayer); // Add it to the current path
         traverseWay(nextLayer, d, network, path, paths); // Go further along the way
       } else { // Not the first output of the current node
         var newPath = JSON.parse(JSON.stringify(path)); // Copy the path, since a new path has been found
         var nextWay = network.layers[u.properties.output[i]]; // Get the next Layer along this way
-        newPath.push(nextWay); // Add the new Layer to the path
         traverseWay(nextWay, d, network, newPath, paths); // Go along this alternate way
       }
     }
