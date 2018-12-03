542ec39c6bd3f73e70581bf2ce4f335ea926ecb4
Alex Workstation <alex.baeuerle@uni-ulm.de>
2018-12-03 16:21:23 +0100

    Duplicates are calculated correctly again.
    To correctly be able to group layers, duplicates are now not dependant on that the input ids of all layers match.

--- netviz/src/groups/Duplicates.js
+++ netviz/src/groups/Duplicates.js
@@ -13,22 +13,10 @@ function groupsEqualLayers(group1, group2) {
   if(group1.layers.length === group2.layers.length) { // Only possible if the groups have the same number of layers
     for (var i in group1.layers) { // For all layers in the Group
       if (group1.layers[i].name === group2.layers[i].name) { // Layers must have the same name
-        if (group1.layers[i].properties.input.length === group2.layers[i].properties.input.length) { // Groups must have the same number of inputs
-          for(var j in group1.layers[i].properties.input) { // For all inputs of the layer
-            if (group1.layers[i].properties.input[j] !== group2.layers[i].properties.input[j]) { // All inputs must be the same
-              return false;
-            }
-          }
-        } else {
+        if (group1.layers[i].properties.input.length !== group2.layers[i].properties.input.length) { // Groups must have the same number of inputs
           return false;
         }
-        if (group1.layers[i].properties.output.length === group2.layers[i].properties.output.length) { // Groups must have the same number of outputs
-          for(var k in group1.layers[i].properties.output) { // For all outputs of the layer
-            if (group1.layers[i].properties.output[k] !== group2.layers[i].properties.output[k]) { // All outputs must be the same
-              return false;
-            }
-          }
-        } else {
+        if (group1.layers[i].properties.output.length !== group2.layers[i].properties.output.length) { // Groups must have the same number of outputs
           return false;
         }
       } else {
