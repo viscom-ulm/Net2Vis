327ac7e43235f8471ddf165dcd49bf59605d3e1b
Alex Workstation <alex.baeuerle@uni-ulm.de>
2018-12-04 18:14:35 +0100

    Color Theme changed
    The color Theme has been changed to a lighter blue.

--- netviz/src/App.js
+++ netviz/src/App.js
@@ -8,25 +8,37 @@ import Controls from './components/controls/ControlsComponent'
 import combinedReducers from './reducers';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import {blue, pink} from '@material-ui/core/colors'
+
 // Create the Store using all the Reducers and applying the Middleware
 const store = createStore(
   combinedReducers,
   applyMiddleware(thunk)
-)
+);
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: pink,
+  },
+});
 
 // Render the App
 // The App provides the Store to the following components.
 // Controls as well as Routed Content are rendered.
 const App = () => (
-  <Provider store={store}>
-    <div className='full'>
-      <CssBaseline />
-      <header>
-        <Controls/>
-      </header>
-      <AppRouter />
-    </div>
-  </Provider>
+  <MuiThemeProvider theme={theme}>
+    <Provider store={store}>
+      <div className='full'>
+        <CssBaseline />
+        <header>
+          <Controls/>
+        </header>
+        <AppRouter />
+      </div>
+    </Provider>
+  </MuiThemeProvider>
 );
 
 export default App;
