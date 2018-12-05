e1d160d512a7de12a00e7c923180345011c81509
Alex Workstation <alex.baeuerle@uni-ulm.de>
2018-12-05 08:28:56 +0100

    Network Preferences moved to own Component
    Created a new Component just for the Network Preferences.

--- netviz/src/components/preferences/PreferencesComponent.js
+++ netviz/src/components/preferences/PreferencesComponent.js
@@ -5,49 +5,15 @@ import {bindActionCreators} from 'redux';
 
 import Typography from '@material-ui/core/Typography';
 
+import NetworkPreferences from './NetworkPreferencesComponent';
+
 import InputField from './InputField'
 import * as actions from '../../actions';
-import Features from './FeaturesComponent';
 import * as removal from '../../groups/Removal';
 import * as activation from '../../groups/Activation';
 
-import * as grouping from '../../groups/Grouping';
-import * as duplicates from '../../groups/Duplicates';
-import * as auto from '../../groups/Automation';
-import * as colors from '../../colors';
-import * as addition from '../../groups/Addition';
-import * as sort from '../../groups/Sort';
-
 // Component for displaying the Preferences of the Visualization
 class Preferences extends React.Component {
-  // Min height of a Layer Changes
-  handleMinChange = (e) => {
-    var preferences = this.props.preferences;
-    preferences.layer_display_min_height.value = parseInt(e.currentTarget.value, 10);
-    this.props.actions.updatePreferences(preferences, this.props.id);
-  }
-
-  // Max Height of a Layer Changes
-  handleMaxChange = (e) => {
-    var preferences = this.props.preferences;
-    preferences.layer_display_max_height.value = parseInt(e.currentTarget.value, 10);
-    this.props.actions.updatePreferences(preferences, this.props.id);
-  }
-
-  // Spacing of the Layers Changes
-  handleSpacingHorizontalChange = (e) => {
-    var preferences = this.props.preferences;
-    preferences.layers_spacing_horizontal.value = parseInt(e.currentTarget.value, 10);
-    this.props.actions.updatePreferences(preferences, this.props.id);
-  }
-  
-  // Spacing of the Layers Changes
-  handleSpacingVerticalChange = (e) => {
-    var preferences = this.props.preferences;
-    preferences.layers_spacing_vertical.value = parseInt(e.currentTarget.value, 10);
-    this.props.actions.updatePreferences(preferences, this.props.id);
-  }
-
   // Called when the Color of the Colorpicker changes
   handleColorChange = (e) => {
     var layerTypes = this.props.layer_types_settings;
@@ -146,60 +112,13 @@ class Preferences extends React.Component {
     this.props.actions.deleteLayerTypes(currLegend, this.props.network, this.props.id); // The Layertypes are done, this is called to update them
   }
 
-  // Group some Layers together
-  groupLayers = () => {
-    this.addGroup(this.props.selection); // Add a new Group based on the current Layer Selection
-  }
-
-  // Automatically Group Layers that are very common in this order
-  autoGroupLayers = () => {
-    var repetition = auto.getMostCommonRepetition(this.props.compressed_network); // Get the most common repetition
-    if (repetition !== undefined) { // If a Repetition could be found
-      this.addGroup(repetition.ids[0]); // Add a Group based on the repetition
-    } else {
-      console.warn('No repetition of at least two layers could be found.');
-    }
-  }
-
-  addGroup = (ids) => {
-    var group = grouping.groupLayers(this.props.compressed_network, ids); // Group the Layers based on given IDs
-    if (group !== undefined && (!duplicates.groupDoesExist(group, this.props.groups))) { // Check if the group could be made and does not already exist
-      var groups = this.props.groups; // Get the current Groups
-      var settings = this.props.layer_types_settings; // Get the current settings
-      settings[group.name] = {
-        color: colors.generateNewColor(settings), // Generate a new Color for the group
-        alias: 'Group' // Initialize the alias
-      }
-      addition.addGroup(groups, group); // Add the new Group to the existing ones
-      sort.sortGroups(groups, settings); // Sort the groups so that the ones that depend on others are at the end
-      this.props.actions.addGroup(groups, this.props.network, settings, this.props.id); // Add the group to the state
-    } else {
-      console.warn('Either a duplicate or no grouping possible.');
-    }
-  }
-
   // Render the Preferences of the Visualization
   render() {
     if(this.props.preferences_toggle) {
       switch (this.props.preferences_mode) {
         case 'network':
           return(
-            <div className='preferencesWrapper'>
-              <div>
-                <Typography variant="h6" color="inherit">
-                  Network
-                </Typography>
-                <InputField value={this.props.preferences.layer_display_min_height.value} type={this.props.preferences.layer_display_min_height.type} description={this.props.preferences.layer_display_min_height.description} action={this.handleMinChange}/>
-                <InputField value={this.props.preferences.layer_display_max_height.value} type={this.props.preferences.layer_display_max_height.type} description={this.props.preferences.layer_display_max_height.description} action={this.handleMaxChange}/>
-                <Features/>
-                <InputField value={this.props.preferences.layers_spacing_horizontal.value} type={this.props.preferences.layers_spacing_horizontal.type} description={this.props.preferences.layers_spacing_horizontal.description} action={this.handleSpacingHorizontalChange}/>
-                <InputField value={this.props.preferences.layers_spacing_vertical.value} type={this.props.preferences.layers_spacing_vertical.type} description={this.props.preferences.layers_spacing_vertical.description} action={this.handleSpacingVerticalChange}/>
-              </div>
-              <div>
-                <InputField value={'Group'} type={'button'} description={'Group'} action={this.groupLayers}/>
-                <InputField value={'Autogroup'} type={'button'} description={'Automatically Group'} action={this.autoGroupLayers}/>
-              </div>
-           </div>
+            <NetworkPreferences/>
           );
         case 'color':
           var group = this.isInGroups(this.props.selected_legend_item);
