4273be7b6cc5556d853b24071a904e9d928b2440
Alex <alex.baeuerle@uni-ulm.de>
2018-12-09 20:16:50 +0100

    Added Color Mode to State
    Users want to choose between picker and a palette. Thus these have been added to the state.

---
+++ netviz/src/reducers/ColorModeReducer.js
@@ -0,0 +1,14 @@
+import initialState from './initialState';
+import * as types from '../actions/types';
+
+export default function colorModeReducer(state = initialState.color_mode, action) {
+  switch (action.type) {
+    case types.SET_SELECTION_COLOR_MODE:
+      return {selection: action.mode, generation: state.generation};
+    case types.SET_GENERATION_COLOR_MODE:
+      return {selection: state.selection, generation: action.mode};
+    default:
+      return state;
+  }
+}
+
