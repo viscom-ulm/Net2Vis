e9c5d8c24ec63442b6177dcc90e4cc7495935103
Alex <alex.baeuerle@uni-ulm.de>
2019-03-31 13:21:47 +0200

    Alert Snack Added
    Whenever the current Code is not Executable, an alter is shown to
    indicate this.

---
+++ net2vis/src/components/AlertSnack.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {bindActionCreators} from 'redux';
+
+import * as actions from '../actions';
+
+import CloseIcon from '@material-ui/icons/Close';
+import { Button, IconButton, Snackbar } from '@material-ui/core';
+
+class AlertSnack extends React.Component {
+  handleClose = (event, reason) => {
+    this.props.actions.updateAlertSnack({open: false, message: ''});
+  };
+
+  render() {
+    return (
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={this.props.alertSnack.open}
+        onClose={this.handleClose}
+        ContentProps={{
+          'aria-describedby': 'message-id',
+        }}
+        message={<span id="message-id">{this.props.alertSnack.message}</span>}
+        action={[
+          <Button key="undo" color="secondary" size="small" onClick={this.handleClose}>
+            Close
+          </Button>,
+          <IconButton key="close" aria-label="Close" color="inherit" onClick={this.handleClose} >
+            <CloseIcon />
+          </IconButton>,
+        ]}
+      />
+    );
+  }
+}
+
+// Prop Types holding all the Preferences
+AlertSnack.propTypes = {
+  alertSnack: PropTypes.object.isRequired
+};
+
+// Map the State to the Properties of this Component
+function mapStateToProps(state, ownProps) {
+  return {
+    alertSnack: state.alert_snack
+  };
+}
+
+// Map the actions of the State to the Props of this Class 
+function mapDispatchToProps(dispatch) {
+  return {actions: bindActionCreators(actions, dispatch)}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AlertSnack);
