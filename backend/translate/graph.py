3fcbdf859525cbd2c2fc18fad72271816d602a08
Alex Bäuerle <alex.baeuerle@uni-ulm.de>
2020-01-07 16:39:03 +0100

    Removed Unused Method in Graph Buildup
    The calculation of layer dimensions is not done by hand anymore.
    However, the method was still there. This method has been removed now.

--- backend/translate/graph.py
+++ backend/translate/graph.py
@@ -1,60 +1,53 @@
 class Node:
-    def __init__(self):
-        self.data = None
-        self.children = []
-        self.parent = None
+  def __init__(self):
+    self.data = None
+    self.children = []
+    self.parent = None
 
 # Representation of the Graph that gets extracted from Code.
 class Graph:
-    def __init__(self):
-        self.layers = []
-        self.root = Node()
-        self.input_shape = []
+  def __init__(self):
+    self.layers = []
+    self.root = Node()
+    self.input_shape = []
 
-    # Add a new Layer to the Graph.
-    def add_layer(self, layer):
-        self.layers.append(layer)
+  # Add a new Layer to the Graph.
+  def add_layer(self, layer):
+    self.layers.append(layer)
 
-    def set_input_shape(self, input_shape):
-        self.input_shape = input_shape
-    
-    # Return the Graph representation.
-    def __repr__(self):
-        str = ''
-        for i in range(len(self.layers)):
-            str = str + type(self.layers[i]).__name__
-            if(i != (len(self.layers)-1)):
-                str = str + ' -> '
-        return str
+  def set_input_shape(self, input_shape):
+    self.input_shape = input_shape
 
-    # Generate references between the Layers based on the Input names.
-    def resolve_input_names(self):
-        for i in range(len(self.layers)):
-            for name in self.layers[i].input_names:
-                for j in range(len(self.layers)):
-                    if(self.layers[j].name == name):
-                        self.layers[i].input.append(self.layers[j])
-                        self.layers[j].output.append(self.layers[i])
+  # Return the Graph representation.
+  def __repr__(self):
+    string = ''
+    for i in range(len(self.layers)):
+      string = string + type(self.layers[i]).__name__
+      if i != (len(self.layers) - 1):
+        string = string + ' -> '
+    return string
 
-    # Calculates the dimensions for each Layer based on an input dimension.
-    def calculate_layer_dimensions(self):
-        input_dim = {
-            'in': self.input_shape,
-            'out': self.input_shape
-        }
+  # Generate references between the Layers based on the Input names.
+  def resolve_input_names(self):
+    for i in range(len(self.layers)):
+      for name in self.layers[i].input_names:
+        for j in range(len(self.layers)):
+          if self.layers[j].name == name:
+            self.layers[i].input.append(self.layers[j])
+            self.layers[j].output.append(self.layers[i])
 
-    # Return the Layer Dimensions as Pretty String.
-    def dimensions_str(self):
-        dim = ''
-        for i in range(len(self.layers)):
-            dim = dim + str(self.layers[i].dimensions)
-            if(i != (len(self.layers)-1)):
-                dim = dim + ' -> '
-        return dim
+  # Return the Layer Dimensions as Pretty String.
+  def dimensions_str(self):
+    dim = ''
+    for i in range(len(self.layers)):
+      dim = dim + str(self.layers[i].dimensions)
+      if(i != (len(self.layers)-1)):
+        dim = dim + ' -> '
+    return dim
 
-    # Return the Layer Dimensions as Array.
-    def dimensions(self):
-        dim = []
-        for i in range(len(self.layers)):
-            dim.append(self.layers[i].dimensions)
-        return dim
+  # Return the Layer Dimensions as Array.
+  def dimensions(self):
+    dim = []
+    for i in range(len(self.layers)):
+      dim.append(self.layers[i].dimensions)
+    return dim
